0033e19b8a274864781de5594639af8719cf87a6
RikDuijm <rikduijm@gmail.com>
2019-12-05 15:33:20 +0000

    if else for databases for travis

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -90,11 +90,11 @@ if "DATABASE_URL" in os.environ:
 else:
     print("Database URL not found. Using SQLite instead")
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
